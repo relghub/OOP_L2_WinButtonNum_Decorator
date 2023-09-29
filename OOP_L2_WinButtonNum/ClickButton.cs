85a2ab42b384ee4ce6231ad839ab2f4ca8c88681
wideprism <wideprism@BOZO>
2023-09-29 23:43:43 +0300

    Add project files.

---
+++ OOP_L2_WinButtonNum/ClickButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_L2_WinButtonNum
+{
+    public class ClickButton : Button
+    {
+        int mClicks;
+        public int Clicks
+        {
+            get { return mClicks; }
+        }
+        protected override void OnClick(EventArgs e)
+        {
+            mClicks++;
+            base.OnClick(e);
+        }
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            System.Drawing.Graphics g = pevent.Graphics;
+            System.Drawing.SizeF stringsize;
+            stringsize = g.MeasureString(Clicks.ToString(),
+                Font, Width);
+            g.DrawString(Clicks.ToString(),Font,
+                System.Drawing.SystemBrushes.ControlText, 
+                Width - stringsize.Width - 3,
+                Height - stringsize.Height - 3);
+        }
+    }
+}
---
+++ OOP_L2_WinButtonNum/Form1.cs
@@ -0,0 +1,10 @@
+namespace OOP_L2_WinButtonNum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
